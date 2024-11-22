f3e0b191a6bd2eb81218caae6a069cdedf60b9d9
Bautista Mercado <bautista.mercado184770@alumnos.info.unlp.edu.ar>
2024-11-22 14:42:21 -0300

    Registro router de tokens

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from app.routes.token import router as token_router
 
 app = FastAPI()
+
+app.include_router(token_router, prefix='/tokens', tags=['tokens'])
 
 @app.get('/')
 def read_root():
     """
     Función de prueba
     """
-    return {"message": "Hola, FastAPI!"}
+    return {"message": "Bienvenido al servicio de generación de tokens."}
